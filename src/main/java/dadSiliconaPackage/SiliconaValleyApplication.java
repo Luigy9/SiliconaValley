a9be7e25f583d9b1c7800dc94b88d9e47ce2698d
Luis <luisalberto.osorio95@gmail.com>
2020-02-04 14:02:16 +0100

    proyecto

---
+++ src/main/java/dadSiliconaPackage/SiliconaValleyApplication.java
@@ -0,0 +1,13 @@
+package dadSiliconaPackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiliconaValleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiliconaValleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dadSiliconaPackage/SiliconaValleyApplicationTests.java
@@ -0,0 +1,13 @@
+package dadSiliconaPackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiliconaValleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
