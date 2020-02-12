2f49be206080513b1e8d7eea105eeef66380a079
jaime <jaime@DESKTOP-1E3KEIM>
2020-02-12 16:53:18 +0100

    clase usuario completada y su repositorio definido

--- src/main/java/repositorios/RepositorioUsuario.java
+++ src/main/java/repositorios/RepositorioUsuario.java
@@ -1,5 +1,11 @@
 package repositorios;
 
-public class RepositorioUsuario {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entidades.Usuario;
+
+public interface RepositorioUsuario extends JpaRepository<Usuario, Long>{
+	
+	Usuario findById(long id);
 
 }
