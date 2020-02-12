1564169fb198380572964e7913799c6622a82254
jaime <jaime@DESKTOP-1E3KEIM>
2020-02-12 15:58:41 +0100

    Creación del resto de clases java necesaria y comienzo de la clase usuario

--- src/main/java/entidades/Usuario.java
+++ src/main/java/entidades/Usuario.java
@@ -1,7 +1,20 @@
 package entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	private String nombreUsuario;
+	private String email;
+	private String direccion;
+	private int telefono;
+	private String nombreCompleto;
 
 }
