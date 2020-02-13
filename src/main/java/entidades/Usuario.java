34afec1b467250b7223c7c7e08bb42b7909da432
jaime <jaime@DESKTOP-1E3KEIM>
2020-02-13 11:40:09 +0100

    usuario atributos bd

--- src/main/java/entidades/Usuario.java
+++ src/main/java/entidades/Usuario.java
@@ -1,9 +1,11 @@
 package entidades;
 
+import java.util.ArrayList;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Usuario {
@@ -17,12 +19,19 @@ public class Usuario {
 	private int telefono;
 	private String nombreCompleto;
 	private int codigoPostal;
+	private String contrasena;
+	@OneToMany(mappedBy="pedido")
+	private ArrayList<Pedido> pedidos = new ArrayList<Pedido>();
+	@OneToMany(mappedBy="cyv")
+	private ArrayList<CyV> cyv = new ArrayList<CyV>();
+	@OneToMany(mappedBy="mensaje")
+	private ArrayList<Mensaje> mensajes = new ArrayList<Mensaje>();
 
 	public Usuario() {
 		
 	}
 	
-	public Usuario(String nombreusuario, String email, String direccion, int telefono, String nombreCompleto, int codigoPostal) {
+	public Usuario(String nombreusuario, String email, String direccion, int telefono, String nombreCompleto, int codigoPostal, String contrasena) {
 
 		this.email = email;
 		this.direccion = direccion;
@@ -30,6 +39,7 @@ public class Usuario {
 		this.nombreUsuario = nombreusuario;
 		this.nombreCompleto = nombreCompleto;
 		this.codigoPostal = codigoPostal;
+		this.contrasena = contrasena;
 		
 	}
 	
@@ -79,6 +89,14 @@ public class Usuario {
 
 	public void setCodigoPostal(Integer codigoPostal) {
 		this.codigoPostal = codigoPostal;
+	}
+	
+	public String getContrasena() {
+		return contrasena;
+	}
+	
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
 	}
 	
 	@Override
