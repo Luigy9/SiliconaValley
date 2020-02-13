57391379791ed0f7994871de0bb606cf97dec30c
danielsantiagom <dany_santy_12@hotmail.com>
2020-02-13 12:24:06 +0100

    Cabeceras del controlador de producto

--- src/main/java/controladores/ControladorProducto.java
+++ src/main/java/controladores/ControladorProducto.java
@@ -1,5 +1,58 @@
 package controladores;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class ControladorProducto {
 
+	@RequestMapping("")//Añadir html donde va 
+	public String agregarProducto( Model model) {
+		
+	}
+	
+	@RequestMapping("")//Añadir html donde va 
+	public String agregarCarrito( Model model) {
+		
+	}
+	
+	@RequestMapping("")//Añadir html donde va 
+	public String modificarProducto( Model model) {
+		
+	}
+	
+	@RequestMapping("")//Añadir html donde va 
+	public String modificarCarrito( Model model) {
+		
+	}
+	
+	@RequestMapping("")//Añadir html donde va 
+	public String buscarCategoriaAsc( Model model) {
+		
+	}
+	
+	@RequestMapping("")//Añadir html donde va 
+	public String buscarCategoriaDesc( Model model) {
+		
+	}
+	
+	@RequestMapping("")//Añadir html donde va 
+	public String buscarPrecioAsc( Model model) {
+		
+	}
+	
+	@RequestMapping("")//Añadir html donde va 
+	public String buscarPrecioDesc( Model model) {
+		
+	}
+	@RequestMapping("")//Añadir html donde va 
+	public String buscarValoracionAsc( Model model) {
+		
+	}
+	
+	@RequestMapping("")//Añadir html donde va 
+	public String buscarValoracionDesc( Model model) {
+		
+	}
+	
+	
 }
