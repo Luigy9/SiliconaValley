cd4618bad394f8028b8caf7a847168e4c513556c
jaime <jaime@DESKTOP-1E3KEIM>
2020-02-14 13:34:18 +0100

    bug de anotación Entity

--- src/main/java/entidades/Mensaje.java
+++ src/main/java/entidades/Mensaje.java
@@ -2,12 +2,14 @@ package entidades;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Mensaje {
 
 	@Id
--- src/main/java/entidades/Pedido.java
+++ src/main/java/entidades/Pedido.java
@@ -1,12 +1,15 @@
 package entidades;
 
 import java.util.ArrayList;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Pedido {
 
 	@Id
