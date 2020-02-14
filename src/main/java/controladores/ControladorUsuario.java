730756a439cd9b63ef0df8cb5f04deb7ff901940
jaime <jaime@DESKTOP-1E3KEIM>
2020-02-14 12:59:09 +0100

    añadiendo mySQL y codigo al primer controlador al proyecto

--- src/main/java/controladores/ControladorUsuario.java
+++ src/main/java/controladores/ControladorUsuario.java
@@ -1,8 +1,23 @@
 package controladores;
 
+import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import entidades.Usuario;
+import repositorios.RepositorioUsuario;
+
+@Controller
 public class ControladorUsuario {
 	
+	@Autowired
+	private RepositorioUsuario repositoriousuario;
 
-
+	@PostConstruct
+	public void init() {
+		
+		Usuario usuario1 = new Usuario("Luis Riaño", "luigys.dad@gmail.com","avenida del tulipan", 913456767, "LUigyy9", 280123, "password");
+		repositoriousuario.save(usuario1);
+	}
 }
