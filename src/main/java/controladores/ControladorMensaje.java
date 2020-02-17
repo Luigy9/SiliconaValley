b1b4cac22d895e0cdceab9b7e4715cf888fbfe4b
danielsantiagom <dany_santy_12@hotmail.com>
2020-02-17 19:59:42 +0100

    Indexacion de shop y contact

--- src/main/java/controladores/ControladorMensaje.java
+++ src/main/java/controladores/ControladorMensaje.java
@@ -2,15 +2,22 @@ package controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import repositorios.RepositorioMensaje;
 
 
+@Controller
 public class ControladorMensaje {
 	
 	@Autowired
 	private RepositorioMensaje repositoriomensaje;
 	
+	@RequestMapping("/accederContacto")
+	public String accederContacto() {
+		
+		return "contact";
+	}
 	
 
 }
