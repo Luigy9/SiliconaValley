6b37afb5574a48bc5cbbb1129beaa3b096b1b003
Luis <luisalberto.osorio95@gmail.com>
2020-03-02 18:47:37 +0100

    creado securityController

---
+++ src/main/java/controladores/securityConfiguration.java
@@ -0,0 +1,20 @@
+package controladores;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+public class securityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests().antMatchers("/index").permitAll();
+		http.authorizeRequests().antMatchers("/login").permitAll();
+		
+	}
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication().withUser("user").password("pass").roles("USER");
+	}
+}
